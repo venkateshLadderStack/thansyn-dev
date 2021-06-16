725c58f478933e61fe433ed4a7ce3e1ade169863
ladderstackankit <ankit@ladderstack.com>
2021-06-16 11:06:05 +0530

    about

---
+++ src/components/AboutCart.js
@@ -0,0 +1,18 @@
+import React from "react"
+
+const AboutCart = () => {
+  return (
+    <div
+      class="col-lg-6 col-md-6"
+      data-aos="fade-right"
+      data-aos-duration="900"
+    >
+      <div class="card-item">
+        <div class="card-content">
+          <h4>{heading}</h4>
+          <p>{description}</p>
+        </div>
+      </div>
+    </div>
+  )
+}
